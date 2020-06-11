45b8f1917eed8d1eca6aee3a6edf5684919ce851
lxfriday <liu3248184446@outlook.com>
2020-06-11 14:24:09 +0800

    utils

--- history-4.10.1/modules/invariant.js
+++ history-4.10.1/modules/invariant.js
@@ -1,1 +1,3 @@
+// invariant(condition, message)
+// condition 为 false，则 throw new Error(`Invariant failed: ${message}`);
 export { default } from 'tiny-invariant';
--- history-4.10.1/modules/warning.js
+++ history-4.10.1/modules/warning.js
@@ -1,1 +1,3 @@
+// warnning(condition, message)
+// codition 为 false，则 console.warn(`Warning: ${message}`)
 export { default } from 'tiny-warning';
